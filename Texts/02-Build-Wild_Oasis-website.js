5d28986fadb07398edff623d6475aa5f6350a009
HarshVardhanK35 <kasavardhan2001@gmail.com>
2025-08-09 23:58:07 +0530

    Project SetUp Completed

--- Texts/02-Build-Wild_Oasis-website.js
+++ Texts/02-Build-Wild_Oasis-website.js
@@ -508,6 +508,258 @@ export default function Logo() {
  * -+-+-+-+-+-+-+-+-+-+-+-+-+-+
  * (Home page: page.js file inside root folder)
  * 
+ * [code]
+ * ------
+import Image from "next/image";
+import Link from "next/link";
+import bgImg from "../public/bg.png";   // - static import
+
+function Home() {
+  return (
+    <main className="mt-24">
+      <Image
+        src={bgImg}   // - specifying image like this will not require [height and width] properties!
+        fill
+        placeholder="blur"      // - this property can only specified with statically loaded images
+        quality={80}        
+        className="object-cover object-top"           // - image occupies whole webpage 
+        alt="Mountains and forests with two cabins"
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </main>
+  );
+}
+export default Home;
+ * 
+ * 
+ *  
+ * ! 10. Building the About Page With Responsive Images
+ * -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+ *  
+ * - about/page.js
+ * [code]
+ * ------
+import Image from "next/image";
+
+import about1 from "../../public/about-1.jpg";
+import about2 from "../../public/about-2.jpg";
+
+export const metadata = {
+  title: "About",
+};
+export default function Page() {
+  return (
+    <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
+      <div className="col-span-3">
+        <h1 className="text-4xl mb-10 text-accent-400 font-medium">
+          Welcome to The Wild Oasis
+        </h1>
+
+        <div className="space-y-8">
+          <p> Where nature's beauty and ... </p>
+          <p> Our 8 luxury cabins ... </p>
+          <p> This is where memorable moments ... </p>
+        </div>
+      </div>
+
+      <div className="col-span-2">
+        <Image
+          src="/about-1.jpg"
+          alt="Family sitting around a fire pit in front of cabin"
+        />
+      </div>
+
+      <div className="col-span-2">
+        <Image src="/about-2.jpg" alt="Family that manages The Wild Oasis" />
+      </div>
+
+      <div className="col-span-3">
+        <h1 className="text-4xl mb-10 text-accent-400 font-medium">
+          Managed by our family since 1962
+        </h1>
+        <div className="space-y-8">
+          <p> Since 1962 ... </p>
+          <p> Over the years ... </p>
+          <div>
+            <a
+              href="/cabins"
+              className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+            >
+              Explore our luxury cabins
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+ * 
+ * - when we reduce the size of browser.. we want to scale images accordingly
+ * [which means we don't set height and weight (images has to set sizes automatically)]
+ * 
+ * - tailwind has set pre-defined sizes for images with "img" tag
+ *    - if we replace with next's Image-comp then we need to specify the height and width!
+ * 
+ * >>> static import
+ * [solves this problem]
+ * ---
+<Image
+  src={about1}    // - imported image statically!
+  alt="Family sitting around a fire pit in front of cabin"
+/>
+ * 
+ * $ WHAT IF?
+ * - when we get an image through a URL from a database
+ *    - as that image does not get statically
+ * 
+ * [about-img-2]
+ * ---
+<div className="col-span-2">
+  <Image src="/about-2.jpg" alt="Family that manages The Wild Oasis" />
+</div>
+ * 
+ * - [then we have to either set height and width OR use "fill" property] 
+ *    - which fills the image on overall web-page
+ * [but we need that image inside a container specified within a GRID and "col-span-2" specified inside parent!]
+ * 
+ * - steps:
+ * ? image need to be inside container
+ * 1: size that container (instead of sizing image >>> make parent: relative)
+ * 2: make that image fit / fill inside that container (then occupies size specified)
+ *    2.1: [using object set to cover: "object-cover"]
+ * 
+ * [code]
+ * ------
+<div className="relative aspect-square col-span-2">   // #1
+  <Image
+    src="/about-2.jpg"
+    fill                        // #2
+    className="object-cover"    // #2.1
+    alt="Family that manages The Wild Oasis"
+  />
+</div>
+ * 
+ * - src gets image via URL from a database!
+ *    - as it is not a static-image 
+ * [we cannot use: "placeholder: blur" and "quality: {1-100}"] >> on dynamic images!  
+ * 
+ * # 1:
+ * <div className="relative aspect-square col-span-2">
+ *    [aspect-ratio: dimensioning parent]
+ * - so we sized the parent div and making image set to div's sizes 
+ * 
+ * # 2 and 2.1:
+ * fill & className="object-cover"
+ *    - we make image inside div to set according to div's sizes
+ * 
+ * [total code specifically with images]
+ * ---
+<div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">   // - making parent "GRID"
+  <div className="col-span-2">
+    <Image
+      src={about1}          // - static img
+      placeholder="blur"    
+      quality={80}
+      alt="Family sitting around a fire pit in front of cabin"
+    />
+  </div>
+
+  <div className="relative aspect-square col-span-2">   // - dimensioning image's parent div
+    <Image  
+      src="/about-2.jpg"    // - dynamic img
+      fill                              // - using FILL and OBJECT-COVER: 
+      className="object-cover"          // - making this image set to parent dimensions
+      alt="Family that manages The Wild Oasis"
+    />
+  </div>
+</div>
+ * 
+ * 
+ * 
+ * ! 11. Adding Nested Routes and Pages
+ * -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 
+ * * segment
+ * http://localhost:3000/account
+ *                      |       |
+ *                      +---+---+
+ *                          |
+ *                          + --- => this is called "SEGMENT"
+ * 
+ * - to create a NESTED-route we have to create a nested-fol inside "account" fol
+ *    - create a new folders: "reservations" and "profile" 
+ * [for routes: /account/reservations AND /account/profile]
+ * 
+ * - inside each folders: "reservations" and "profile"
+ *    - create a page.js inside these!
+ * 
+ * $ PROBLEM
+ * - we don't have navigation to /account/reservations and /account/profile..
+ *    - inside the page: /account
+ * [so we set up nested navigation]
+ * 
+ * 
+ * next lecture.. 
+ * => nested layout
+ * 
+ * 
+ * ! 12.  Adding a Nested Layout
+ * -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * (nested layout with NEXT.JS by adding a side navigation to all the routes in account-page/route)
+ * 
+ * - when we want a common / global layout for a set of routes in a folder
+ *    - then we should follow a convention to create a file with name: "layout.js" 
+ * 
+ * - here for /account AND /account/reservations AND /account/profile
+ *    - so we want to have a common navigation for all these three!
+ * 
+ * >>> [so we have to create layout.js file in common "/account" folder!]
+ * 
+ * $ SIMILAR
+ * - we added a common / global layout to every route inside root folder.. 
+ *    - in which this global layout will be applied for every folder that we create inside root folder!
+ * [every folder inside root folder => new route inside a URL] 
+ * ---
+// >>> folder and files structure
+---
+--- app
+
+// COMMON_CODE
+  --- [_components]-fol
+    -
+  --- [_lib]-fol
+    -
+  --- [_styles]-fol
+    - global.css
+
+// ROUTES
+  --- [about]-fol
+    - page.js
+  --- [cabins]-fol
+    - page.js
+  
+  --- [account]-fol
+    - page.js
+    --- [reservations]-fol
+      - page.js
+    --- [profile]-fol
+      - page.js
+  
+// GLOBALS
+  --- layout.js     - GLOBAL LAYOUT
+  --- loading.js
+  --- page.js       - HOME
  * 
  * 
  * 
@@ -521,24 +773,6 @@ export default function Logo() {
  * 
  * 
  * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * ! 2. Project Planning: "The Wild Oasis" Customer Website
- * -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
- * 
- * 
- * ! 2. Project Planning: "The Wild Oasis" Customer Website
- * -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
- * 
- * 
- * ! 2. Project Planning: "The Wild Oasis" Customer Website
- * -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
  * 
  * 
  * ! 2. Project Planning: "The Wild Oasis" Customer Website
